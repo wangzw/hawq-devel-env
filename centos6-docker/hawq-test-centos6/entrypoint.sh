0a215a2d2b33c5398fe3e81162ce5b1eec4edfa6
Zhanwei Wang <wangzw@wangzw.org>
2016-01-06 16:45:54 +0800

    add python module

--- centos6-docker/hawq-test-centos6/entrypoint.sh
+++ centos6-docker/hawq-test-centos6/entrypoint.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
-/etc/init.d/sshd start
-
 if [ -z "${NAMENODE}" ]; then
   export NAMENODE=${HOSTNAME}
 fi
 
 if [ ! -f /etc/profile.d/hadoop.sh ]; then
-  echo "#!/bin/bash" > /etc/profile.d/hadoop.sh
-  echo "export NAMENODE=${NAMENODE}" >> /etc/profile.d/hadoop.sh
-  chmod a+x /etc/profile.d/hadoop.sh
+  echo '#!/bin/bash' | sudo tee /etc/profile.d/hadoop.sh
+  echo "export NAMENODE=${NAMENODE}" | sudo tee -a /etc/profile.d/hadoop.sh
+  sudo chmod a+x /etc/profile.d/hadoop.sh
 fi
 
-if [ "${NAMENODE}" == "${HOSTNAME}" ]; then
-  if [ ! -d /tmp/hdfs/name/current ]; then
-    su -l hdfs -c "hdfs namenode -format"
-  fi
-  
-  if [ -z "`ps aux | grep org.apache.hadoop.hdfs.server.namenode.NameNode | grep -v grep`" ]; then
-    su -l hdfs -c "hadoop-daemon.sh start namenode"
-    sudo -u hdfs hdfs dfs -chmod 777 /
-  fi
-else
-  if [ -z "`ps aux | grep org.apache.hadoop.hdfs.server.datanode.DataNode | grep -v grep`" ]; then
-    su -l hdfs -c "hadoop-daemon.sh start datanode"
-  fi
-fi
-
-su - gpadmin
+sudo start-hdfs.sh
 
 exec "$@"
---
+++ centos6-docker/hawq-test-centos6/start-hdfs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+/etc/init.d/sshd start
+
+if [ -f /etc/profile.d/hadoop.sh ]; then
+  . /etc/profile.d/hadoop.sh
+fi
+
+if [ "${NAMENODE}" == "${HOSTNAME}" ]; then
+  if [ ! -d /tmp/hdfs/name/current ]; then
+    su -l hdfs -c "hdfs namenode -format"
+  fi
+  
+  if [ -z "`ps aux | grep org.apache.hadoop.hdfs.server.namenode.NameNode | grep -v grep`" ]; then
+    su -l hdfs -c "hadoop-daemon.sh start namenode"
+    sudo -u hdfs hdfs dfs -chmod 777 /
+  fi
+else
+  if [ -z "`ps aux | grep org.apache.hadoop.hdfs.server.datanode.DataNode | grep -v grep`" ]; then
+    su -l hdfs -c "hadoop-daemon.sh start datanode"
+  fi
+fi
+
